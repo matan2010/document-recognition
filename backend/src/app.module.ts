67860bbec7129931bf2f7bd3440243145572bd22
NoamSfez <noam.sfez@gmail.com>
2024-12-19 20:50:24 +0200

    integrate document and client modules to nest app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { CompaniesModule } from './companies/companies.module';
+import { DocumentsModule } from './documents/documents.module';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { CompaniesModule } from './companies/companies.module';
     PrismaModule,
     AuthModule,
     CompaniesModule,
+    DocumentsModule,
+    ClientsModule
   ],
 })
 export class AppModule {}
