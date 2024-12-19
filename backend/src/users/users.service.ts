80093d4a7a746d5b1f8db57eac3e8ce89fb7a6fb
NoamSfez <noam.sfez@gmail.com>
2024-12-19 23:02:59 +0200

    add user module

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,72 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto, companyId: string) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+        companyId
+      },
+    });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findAllByCompany(companyId: string) {
+    return this.prisma.user.findMany({
+      where: { companyId }
+    });
+  }
+
+  async findOne(id: string, companyId: string) {
+    const user = await this.prisma.user.findFirst({
+      where: { 
+        id,
+        companyId
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User not found`);
+    }
+
+    return user;
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto, companyId: string) {
+    // If password is being updated, hash it
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+    }
+
+    return this.prisma.user.update({
+      where: { 
+        id,
+        companyId
+      },
+      data: updateUserDto,
+    });
+  }
+
+  async remove(id: string, companyId: string) {
+    const deletedUser = await this.prisma.user.delete({
+      where: { 
+        id,
+        companyId
+      },
+    });
+    
+    return { deletedUser };
+  }
+}
